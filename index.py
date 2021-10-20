0dbc3bf71dbad6e92031a600d4b36204df044350
WoodyAhn <jayahn0104@gmail.com>
2021-10-20 14:20:15 +0900

    make it better

--- index.py
+++ index.py
@@ -7,14 +7,14 @@ from app import app
 from app import server
 
 # Connect to your app pages
-from apps import vgames, global_sales, one_product, market
+from apps import vgames, global_sales, one_product
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div([
         dcc.Link('Video Games|', href='/apps/vgames'),
         dcc.Link('Other Products|', href='/apps/global_sales'),
-        dcc.Link('전체 시장|', href='/apps/market'),
+        # dcc.Link('전체 시장|', href='/apps/market'),
         dcc.Link('개별 품목', href='/apps/one_product')
     ], className="row"),
     html.Div(id='page-content', children=[])
@@ -28,12 +28,12 @@ def display_page(pathname):
         return vgames.layout
     if pathname == '/apps/global_sales':
         return global_sales.layout
-    if pathname == '/apps/market':
-        return market.layout
+    # if pathname == '/apps/market':
+    #     return market.layout
     if pathname == '/apps/one_product':
         return one_product.layout
     else:
-        return market.layout
+        return one_product.layout
 
 
 if __name__ == '__main__':
