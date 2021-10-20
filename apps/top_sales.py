773331f0d13683644ed6fbb43e9ed5c9d37d9520
WoodyAhn <jayahn0104@gmail.com>
2021-10-20 17:01:20 +0900

    make it better

--- apps/top_sales.py
+++ apps/top_sales.py
@@ -7,37 +7,39 @@ from app import app
 import plot_fn as pf
 import plotly.express as px
 
-def df_top(df, var, top_limit):
-    sorted_df = df.sort_values(by=var, ascending=False)
-    target_df = sorted_df[:top_limit]
-    target_other_df = pd.DataFrame(sorted_df[top_limit:].sum()).transpose()
-    out_df = pd.concat([target_df, target_other_df], axis=0)
-    out_df.rename({0:'Others'}, inplace=True)
-    return out_df
+# def df_top(df, var, top_limit):
+#     sorted_df = df.sort_values(by=var, ascending=False)
+#     target_df = sorted_df[:top_limit]
+#     target_other_df = pd.DataFrame(sorted_df[top_limit:].sum()).transpose()
+#     out_df = pd.concat([target_df, target_other_df], axis=0)
+#     out_df.rename({0: 'Others'}, inplace=True)
+#     return out_df
+#
+#
+# def df_top_years(df, var, top_limit, others_drop=True):
+#     df_list = []
+#     for year in df.unstack().index:
+#         year_df = df.xs(year)
+#         df_list.append(df_top(year_df, var, top_limit)[var])
+#     out_df = pd.concat(df_list, axis=1)
+#     out_df.set_axis(df.unstack().index, axis=1, inplace=True)
+#     if others_drop: out_df.drop('Others', axis=0, inplace=True)
+#     return out_df
+#
+#
+# def df_top_reduce(df, top_list):
+#     top_df = df[df['pumName'].isin(top_list)]
+#     other_df = df[df['pumName'].isin(top_list)].groupby('Year').sum()
+#     other_df['pumName'] = 'others'
+#     other_df['Year'] = other_df.index
+#
+#     return pd.concat([top_df, other_df], axis=0)
 
-
-def df_top_years(df, var, top_limit, others_drop=True):
-    df_list = []
-    for year in df.unstack().index:
-        year_df = df.xs(year)
-        df_list.append(df_top(year_df, var, top_limit)[var])
-    out_df = pd.concat(df_list, axis=1)
-    out_df.set_axis(df.unstack().index, axis=1, inplace=True)
-    if others_drop: out_df.drop('Others', axis=0, inplace=True)
-    return out_df
-
-
-def df_top_reduce(df, top_list):
-    top_df = df[df['pumName'].isin(top_list)]
-    other_df = df[df['pumName'].isin(top_list)].groupby('Year').sum()
-    other_df['pumName'] = 'others'
-    other_df['Year'] = other_df.index
-
-    return pd.concat([top_df, other_df], axis=0)
 
 # get relative data folder
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("../datasets").resolve()
+# DATA_PATH = PATH.joinpath("../Deploy/datasets").resolve()
 
 df = pd.read_csv(DATA_PATH.joinpath("2017_2021_flower.csv"), encoding='euc-kr', index_col=0)
 
@@ -86,11 +88,11 @@ layout = html.Div([
 
 @app.callback(
     Output(component_id='out-fig-top-bar', component_property='figure'),
-    [Input(component_id='input-top-var', component_property='value'),
+    [Input(component_id='input-var', component_property='value'),
      Input(component_id='input-top-limit', component_property='value')]
 )
 def update_graph(var, top_limit):
-    top_list = df_top_years(year_pum_df, var, top_limit, others_drop=False).index
-    fin_df = df_top_reduce(year_pum_df, top_list)
+    top_list = pf.df_top_years(year_pum_df, var, top_limit, others_drop=False).index
+    fin_df = pf.df_top_reduce(year_pum_df, top_list)
     fig = px.bar(fin_df.sort_values(by=var, ascending=False), x='Year', y=var, color='pumName')
     return fig
